f5646170e9702a6c6c363de5193eaf1e8c5172fe
HongfanChen <71697359+HongfanChen@users.noreply.github.com>
2021-09-24 11:11:55 -0400

    add vars to dplyr select semantics
    add vars to dplyr select semantics

--- R/set_null.R
+++ R/set_null.R
@@ -1,2 +1,5 @@
 
-AE_NAME <- DRUG_TYPE <- Freq <-  GROUP_NAME <- OR <- Ratio <- n_AE <- p_value <- qval <- isRatio0 <- sig <- NULL
+AENo <- AEYes <- count <- AE_NAME <- DRUG_TYPE <- Freq <- GROUP_NAME <- NULL
+YES <- ID <- OR <- Ratio <- n_AE <- p_value <- qval <- isRatio0 <- sig <- NULL
+NO <- miss <- OR_p <- N_M <- hit <- BETA <- `95Lower` <- `95Upper` <- NULL
+ES <- group <- GROUP_SIZE <- Nr <- `se(logOR)`<- NULL
