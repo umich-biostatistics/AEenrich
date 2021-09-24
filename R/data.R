e143fb6d21bc3ac7ebf19963b79353bf0de24168
HongfanChen <71697359+HongfanChen@users.noreply.github.com>
2021-09-23 23:51:23 -0400

    update example data
    substitute flu data by covid data

--- R/data.R
+++ R/data.R
@@ -1,29 +1,34 @@
 
-#' Flu Vaccine Adverse Event Data
+#' Covid Vaccine Adverse Event Data
 #'
 #' Adverse event data in the long format. Each row is a single adverse 
-#' event.
+#' event, along with covariates.
 #' 
 #' \itemize{
 #'   \item VAERS_ID Event ID
-#'   \item VAX_TYPE Vaccine type
+#'   \item VAX_LABEL Vaccine type
 #'   \item AE_NAME Adverse event name
+#'   \item AGE covariate
+#'   \item SEX covariate
 #' }
 #'
-"flu1"
+"covid1"
 
-#' Flu Vaccine Adverse Event Data
+#' Covid Vaccine Adverse Event Data
 #'
 #' Adverse event data in the short format. Each row is a count of adverse 
 #' events with the given name.
 #' 
 #' \itemize{
-#'   \item VAX_TYPE Vaccine type
+#'   \item DRUG_TYPE Vaccine type
 #'   \item AE_NAME Adverse event name
-#'   \item Count Frequency of adverse event
+#'   \item AEYes Number of observations that have this AE
+#'   \item AENo Number of observations that do not have this AE
+#'   \item AGE covariate
+#'   \item SEX covariate
 #' }
 #'
-"flu2"
+"covid2"
 
 #' Group Structure Data
 #'
