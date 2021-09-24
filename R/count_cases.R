2b1fb1b910c14f63af9c2ef028aba5054fbed990
HongfanChen <71697359+HongfanChen@users.noreply.github.com>
2021-09-24 11:20:55 -0400

    escape special symbols

--- R/count_cases.R
+++ R/count_cases.R
@@ -12,10 +12,10 @@
 #' @param covar_disc a character vector of categorical covariates.
 #' @param breaks a list consists of vectors used for creating specific bins to
 #' transform continuous covariates into categorical. Breaks Should have the same
-#' length as covar_cont. Given a vector of non-decreasing breakpoints in breaks[i],
-#' find the interval containing each element of covar_cont[i]; i.e., for each index
-#' j in breaks[i], value j is assigned to covar_cont[i] if and only if breaks[i][j]
-#' <= covar_cont[i] < breaks[i][j+1].
+#' length as covar_cont. Given a vector of non-decreasing breakpoints in `breaks[i]`,
+#' find the interval containing each element of `covar_cont[i]`; i.e., for each index
+#' j in `breaks[i]`, value j is assigned to `covar_cont[i]` if and only if `breaks[i][j]`
+#' `\leq covar_cont[i] < breaks[i][j+1]`.
 #' @param min_AE the minimum number of cases required to start counting
 #' for a specific AE. Default 10.
 #' @param cores the number of cores to use for parallel execution.
